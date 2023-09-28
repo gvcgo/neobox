b2142e80ad1f4259366a8099e88968e9d2a82d2d
moqsien <moqsien@foxmail.com>
2023-09-28 18:02:40 +0800

    config update

--- pkgs/client/cnf.go
+++ pkgs/client/cnf.go
@@ -31,7 +31,7 @@ var XrayCoreConfigStr = `{
         }
     ],
     "log": {
-        "loglevel": "warning",
+        "loglevel": "error",
         "error": ""
     },
     "outbounds": [
@@ -100,101 +100,6 @@ var XrayCoreConfigStr = `{
         ]
     }
 }`
-
-/*
-
-var SingBoxConfigStr string = `{
-    "log": {
-        "disabled": false,
-        "level": "error",
-        "timestamp": true
-    },
-    "dns": {
-        "servers": [
-            {
-                "tag": "google",
-                "address": "8.8.8.8"
-            },
-            {
-                "tag": "NeuStar",
-                "address": "156.154.70.1"
-            },
-            {
-                "tag": "Norton",
-                "address": "199.85.126.30"
-            },
-            {
-                "tag": "cloudflare",
-                "address": "1.1.1.1"
-            },
-            {
-                "tag": "china",
-                "address": "local",
-                "detour": "direct"
-            }
-        ],
-        "rules": [
-            {
-                "geosite": "cn",
-                "server": "china"
-            },
-            {
-                "domain_keyword": [
-                    "google",
-                    "github",
-                    "youtube"
-                ],
-                "server": "NeuStar"
-            }
-        ],
-        "disable_cache": true,
-        "disable_expire": true
-    },
-    "inbounds": [
-        {
-            "type": "mixed",
-            "tag": "mixed-in",
-            "listen": "::",
-            "listen_port": 2019,
-            "sniff": true,
-            "set_system_proxy": false,
-            "domain_strategy": "prefer_ipv4"
-        }
-    ],
-    "outbounds": [
-        {},
-        {
-            "type": "direct",
-            "tag": "direct"
-        },
-        {
-            "type": "block",
-            "tag": "block"
-        }
-    ],
-    "route": {
-        "rules": [
-            {
-                "geosite": "cn",
-                "geoip": "cn",
-                "outbound": "direct"
-            },
-            {
-                "geosite": "category-ads-all",
-                "outbound": "block"
-            }
-        ],
-        "auto_detect_interface": true,
-        "final": "PROXY_OUT",
-        "geoip": {
-            "path": ""
-        },
-        "geosite": {
-            "path": ""
-        }
-    }
-}`
-*/
 
 var SingBoxConfigString string = `{
     "dns":{
